6fab17790db5450fad7e7890cd8a5fc0460d97a5
random0712 <patricknuvem9@gmail.com>
2021-12-04 02:27:24 -0300

    Creating entities

---
+++ src/main/java/com/patrickamaro/bookstoremanager/users/entity/User.java
@@ -0,0 +1,43 @@
+package com.patrickamaro.bookstoremanager.users.entity;
+
+import com.patrickamaro.bookstoremanager.books.entity.Book;
+import com.patrickamaro.bookstoremanager.users.enums.Gender;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = false, length = 20)
+    private Gender gender;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Book> books;
+}
