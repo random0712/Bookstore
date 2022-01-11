ab1df5af2ed61c2e30f6b1e1d35b1f7765b2a86b
random0712 <patricknuvem9@gmail.com>
2022-01-11 13:53:36 -0300

    feature(DTO): created author DTO class

---
+++ src/main/java/com/patrickamaro/bookstoremanager/author/dto/AuthorDTO.java
@@ -0,0 +1,31 @@
+package com.patrickamaro.bookstoremanager.author.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Max(120)
+    private Integer age;
+}
