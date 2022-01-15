71251e4e8cb197c223461bce27041ca229875832
random0712 <patricknuvem9@gmail.com>
2022-01-15 16:05:44 -0300

    Created base authors structure classes

---
+++ src/main/java/com/patrickamaro/bookstoremanager/author/controller/AuthorControllerDocs.java
@@ -0,0 +1,7 @@
+package com.patrickamaro.bookstoremanager.author.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "AuthorController", description = "Author management")
+public interface AuthorControllerDocs {
+}
