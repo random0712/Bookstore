359da67ac5d80e076ad6bf7a89a13650533405f5
random0712 <patricknuvem9@gmail.com>
2022-01-16 03:27:19 -0300

    developed author creation funcionality

--- src/main/java/com/patrickamaro/bookstoremanager/author/controller/AuthorControllerDocs.java
+++ src/main/java/com/patrickamaro/bookstoremanager/author/controller/AuthorControllerDocs.java
@@ -1,7 +1,20 @@
 package com.patrickamaro.bookstoremanager.author.controller;
 
+import com.patrickamaro.bookstoremanager.author.dto.AuthorDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 @Tag(name = "AuthorController", description = "Author management")
 public interface AuthorControllerDocs {
+    @Operation(summary = "Author creation operation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Success author creation"),
+            @ApiResponse(responseCode = "400", description = "Missing required fields, wrong field range value or author already registered on system")
+    })
+    public AuthorDTO create( AuthorDTO authorDTO);
 }
--- src/test/java/com/patrickamaro/bookstoremanager/author/service/AuthorServiceTest.java
+++ src/test/java/com/patrickamaro/bookstoremanager/author/service/AuthorServiceTest.java
@@ -1,13 +1,24 @@
 package com.patrickamaro.bookstoremanager.author.service;
 
 import com.patrickamaro.bookstoremanager.author.builder.AuthorDTOBuilder;
+import com.patrickamaro.bookstoremanager.author.dto.AuthorDTO;
+import com.patrickamaro.bookstoremanager.author.entity.Author;
 import com.patrickamaro.bookstoremanager.author.mapper.AuthorMapper;
 import com.patrickamaro.bookstoremanager.author.repository.AuthorRepository;
+import com.patrickamaro.bookstoremanager.exception.AuthorAlreadyExistsException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorServiceTest {
@@ -25,5 +36,29 @@ public class AuthorServiceTest {
     @BeforeEach
     void setUp() {
         authorDTOBuilder = AuthorDTOBuilder.builder().build();
+    }
+
+    @Test
+    void whenNewAuthorIsInformedThenItShouldBeCreated() {
+        AuthorDTO expectedAuthorToCreateDTO = authorDTOBuilder.buildAuthorDTO();
+        Author expectedCreatedAuthor = authorMapper.toModel(expectedAuthorToCreateDTO);
+
+        when(authorRepository.save(expectedCreatedAuthor)).thenReturn(expectedCreatedAuthor);
+        when(authorRepository.findByName(expectedAuthorToCreateDTO.getName())).thenReturn(Optional.empty());
+
+        AuthorDTO createdAuthorDTO = authorService.create(expectedAuthorToCreateDTO);
+
+        assertThat(createdAuthorDTO, equalTo(expectedAuthorToCreateDTO));
+    }
+
+    @Test
+    void whenExistingAuthorIsInformedThenAnExceptionShouldBeThrown() {
+        AuthorDTO expectedAuthorToCreateDTO = authorDTOBuilder.buildAuthorDTO();
+        Author expectedCreatedAuthor = authorMapper.toModel(expectedAuthorToCreateDTO);
+
+        when(authorRepository.findByName(expectedAuthorToCreateDTO.getName()))
+                .thenReturn(Optional.of(expectedCreatedAuthor));
+
+        assertThrows(AuthorAlreadyExistsException.class, () -> authorService.create(expectedAuthorToCreateDTO));
     }
 }
